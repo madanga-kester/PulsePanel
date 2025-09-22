99ae566fb81e7ca6347029ee57c38603cf3d0b44
Kester Madanga <madangakester@gmail.com>
2025-09-22 19:07:49 +0300

    Add project files.

---
+++ PulsePanel/Models/ClickEvents.cs
@@ -0,0 +1,69 @@
+﻿namespace PulsePanel.Models
+{
+    public class ClickEvents
+    {
+
+        public string ElementId { get; set; }      // Taking which button or link was clicked
+        public DateTime Timestamp { get; set; }    // showing  When it happened
+        public string PageUrl { get; set; }        // Where the click happened
+        public string UserId { get; set; }         // to identify  the user session
+
+        public string IpAddress { get; set; }      // User's IP
+        public string UserDevice { get; set; }      // Browser/device info
+        public string PreviousPageUrl { get; set; }    // Previous page
+
+
+
+
+
+        public ClickEvents(string aElementId, string aPageUrl, string aUserId, string aIpAddress, string aUserDevice, string aPreviousPageUrl)
+        {
+            ElementId = aElementId;
+            PageUrl = aPageUrl;
+            UserId = aUserId;
+            Timestamp = DateTime.UtcNow;
+
+            IpAddress = aIpAddress;
+            UserDevice = aUserDevice;
+            PreviousPageUrl = aPreviousPageUrl;
+
+
+        }
+
+        //methods
+        //checking whether the click happened withing the last seconds
+
+
+        public bool RecentClick(int seconds)
+        {
+            return (DateTime.UtcNow - Timestamp).TotalSeconds <= seconds;
+        }
+
+        public override string ToString()
+        {
+            return $"[ClickEvents] ElementId: {ElementId}, PageUrl: {PageUrl}, UserId: {UserId}, Timestamp: {Timestamp}, IpAddress: {IpAddress}, UserDevice: {UserDevice}, PreviousPageUrl: {PreviousPageUrl}";
+        }
+
+
+        //  storing   multiple clicks
+        public class ClickEventManager
+        {
+            private List<ClickEvents> _clickHistory = new List<ClickEvents>();
+
+            // Adding a new click event
+            public void AddClick(string elementId, string pageUrl, string userId, string IpAddress, string UserDevice, string PreviousPageUrl)
+            {
+                var click = new ClickEvents(elementId, pageUrl, userId, IpAddress, UserDevice, UserDevice);
+                _clickHistory.Add(click);
+            }
+
+            // Getting all clicks for a given user
+            public List<ClickEvents> GetUserClicks(string userId)
+            {
+                return
+            }
+
+
+        }
+    }
+}
